8d8a58d530544861c6b0fef86abfddbd111cfc0c
deepika-sivakumar <50595102+deepika-sivakumar@users.noreply.github.com>
2019-06-29 13:58:55 -0400

    Update BagLearner.py

--- code/BagLearner.py
+++ code/BagLearner.py
@@ -27,7 +27,7 @@ class BagLearner(object):
             self.learnerEnsemble.append(self.learner(**self.kwargs))
 
     def author(self):
-        return 'dsivakumar6' # Georgia Tech username
+        return 'Deepika'
 
     def addEvidence(self,dataX,dataY):
         # Concatenate the two arrays
@@ -61,4 +61,4 @@ class BagLearner(object):
         return Y
 
 if __name__=="__main__":
-    print "the secret clue is 'zzyzx'"
+    print "Bag Learner"
